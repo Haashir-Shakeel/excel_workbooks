e70e10ac7a5b93a5c25ce51b0808e489fdf8a63a
Haashir-Shakeel <david10james99@gmail.com>
2022-07-05 17:15:48 +0500

    searching in multiple files

--- search_excel.py
+++ search_excel.py
@@ -9,3 +9,11 @@ df = pd.read_excel(excel_file)
 
 scorer = df['Name'].where(df['Test 1']==74)
 print(scorer.dropna())
+
+excel_files = ['sample_scores.xlsx','sample_scores2.xlsx','sample_scores3.xlsx']
+for individual_excel_file in excel_files:
+    df = pd.read_excel(individual_excel_file)
+    scorer = df['Name'].where(df['Test 1']==74).dropna()
+
+    print('File Name:' + individual_excel_file)
+    print(scorer)
